ffe697aa8e96e11155d55ef1e0e552547a5c839c
Dmitri Smirnov <smirnov.dmitri@gmail.com>
2022-10-01 18:35:30 +0300

    Second solution for pets.

--- pet.go
+++ pet.go
@@ -2,6 +2,28 @@
 package pet
 
 // Pet defines pet type.
-type Pet interface {
+type Pet struct {
+	Name  string
+	Years int
+}
+
+// Vet defines interface for pets.
+type Vet interface {
 	HumanYears() int
+}
+
+// Dog defines type of a dog.
+type Dog Pet
+
+// Cat defines type of a cat.
+type Cat Pet
+
+// HumanYears converts human years to dog years.
+func (d Dog) HumanYears() int {
+	return d.Years * 7
+}
+
+// HumanYears converts human years to cat years.
+func (c Cat) HumanYears() int {
+	return c.Years * 10
 }
--- pet_test.go
+++ pet_test.go
@@ -3,32 +3,46 @@ package pet
 import "testing"
 
 type testpaircat struct {
-	Cat
+	Vet
 	answer int
 }
 
 func TestingDogYears(t *testing.T) {
 	pairs := []testpaircat{
 		{
-			Cat: Cat{
+			Vet: Cat{
 				Name:  "Barsik",
 				Years: 3,
 			},
 			answer: 30,
 		},
 		{
-			Cat: Cat{
+			Vet: Cat{
 				Name:  "Murka",
 				Years: 10,
 			},
 			answer: 100,
+		},
+		{
+			Vet: Dog{
+				Name:  "Bobik",
+				Years: 5,
+			},
+			answer: 35,
+		},
+		{
+			Vet: Dog{
+				Name:  "Barboss",
+				Years: 9,
+			},
+			answer: 63,
 		},
 	}
 
 	for _, p := range pairs {
 		if p.HumanYears() != p.answer {
 			t.Error(
-				"for", p.Cat,
+				"for", p.Vet,
 				"expected", p.answer,
 				"got", p.HumanYears(),
 			)
